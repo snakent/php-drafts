faeda18cdb690949423d63fc50255ab8be2c5e26
snakent <tradent@mail.ru>
2019-12-04 01:25:33 +0500

    Update drafts.php

--- drafts.php
+++ drafts.php
@@ -1,3 +1,11 @@
 <?php
-  /* Leave only numbers */
-  $str = preg_replace("/[^0-9]/", '', $str);
+    /* Leave only numbers */
+    $str = "asdkE2oedsl;043w";
+    $str = preg_replace("/[^0-9]/", "", $str);
+    echo $str; //result is 2043
+
+    /* Contains string with slashes */
+    $url = "/catalog/23/";
+    if(preg_match("/\/catalog\//", $url, $match)) {
+        echo $match[0]; //result is /catalog/
+    };
