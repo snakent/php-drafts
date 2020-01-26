9e067c304deb48c7385e5ffbeac3f84631991438
snakent <tradent@mail.ru>
2020-01-26 21:30:08 +0500

    Update drafts.php

--- drafts.php
+++ drafts.php
@@ -9,3 +9,13 @@
     if(preg_match("/\/catalog\//", $url, $match)) {
         echo $match[0]; //result is /catalog/
     };
+
+    /* Replace all spaces to one space */
+    $str = "Audi A4 B8 big    with hydro ";
+    $str = preg_replace('| +|', ' ', trim($str));
+    var_dump($str); //result is "Audi A4 B8 big with hydro"
+
+    /* Replace all spaces and space symbols to one space */
+    $str = "Audi A4 B8 \t\t big  \t\n  with hydro ";
+    $str = preg_replace('|\s+|', ' ', trim($str));
+    var_dump($str); //result is "Audi A4 B8 big with hydro"
